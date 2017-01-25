a1fa2bfcd3295a478a20e0a63fdbe98e8a727d24
Steven Thompson <steventhompson@Stevens-MacBook-Pro.local>
2017-01-25 09:29:34 -0700

    econd try

--- NetBeansProjects/MDSimulation/src/mdsimulation/Driver0.java
+++ NetBeansProjects/MDSimulation/src/mdsimulation/Driver0.java
@@ -7,26 +7,23 @@ public class Driver0 {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to Project 0.");
-        System.out.println("What is the name of the data file? ");
-        //String file = input.nextLine();
         readFile("/Users/steventhompson/Desktop/input.txt");
     }
 
     public static void readFile(String file) {
         try {
             Scanner sc = new Scanner(new File(file));
-            dimension1 = 206;
+            dimension1 = sc.nextInt();
             dimension2 = 9;
             array = new double[dimension1][dimension2];
-            System.out.println("test2");
+           
             while (sc.hasNext()) {
                 for (int row = 0; row < dimension1; row++) {
-                    System.out.println("test3");
+                    
                     for (int column = 0; column < dimension2; column++) {
                         array[row][column] = sc.nextDouble();
-                        System.out.println("test4");
-                        System.out.println(array[row][column]);
+                        
+                        System.out.print(array[row][column] + " ");
                     }
                     
                     System.out.println("test");
