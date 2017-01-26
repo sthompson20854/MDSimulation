5e2e94968c5682971493e29f9b19d8d15d45f381
Steven Thompson <steventhompson@Stevens-MacBook-Pro.local>
2017-01-26 15:45:22 -0700

    third

--- NetBeansProjects/MDSimulation/src/mdsimulation/Event.java
+++ NetBeansProjects/MDSimulation/src/mdsimulation/Event.java
@@ -17,21 +17,7 @@ public class Event {
    double event_x;
    double event_y;
    
-   double delta_x;
-   double delta_y;
-   double[] total_distance;
-   
-   double delta_vx;
-   double delta_vy;
-   double[] diffrence_velocity;
-   
-   double r_r;
-   double v_v;
-   double v_r;
-   
-   double total_radius; //sigma
-   
-   double d;
+  
    
    public Event(long time_event, Particle particle, Particle other, double event_x, double event_y)
    {
@@ -42,23 +28,7 @@ public class Event {
        this.time_event = time_event;
        
        
-       delta_x = (particle.rx - other.rx); //distance apart in x 
-       delta_y = (particle.ry - other.ry); // distance apart in y
-       total_distance[0] = (delta_x);
-       total_distance[1] = (delta_y);
-       
-       delta_vx = (particle.vx - other.vx); //diffrecne in particles x velocity
-       delta_vy = (particle.vy - other.vy); //diffrecne in particles y velocity;
-      diffrence_velocity[0] = (delta_vx);
-      diffrence_velocity[1] = (delta_vy);
       
-      r_r = (delta_x)*(delta_x) + delta_y *delta_y ;
-      v_v = (delta_vx)*(delta_vx) + delta_vy *delta_vy ;
-      v_r =(delta_vx)*(delta_x) + delta_vy *delta_y;
-      
-      total_radius = particle.radius + other.radius;
-      
-      d = (v_r * v_r) - (v_v) * (r_r - (total_radius *total_radius));
    }
    
 
