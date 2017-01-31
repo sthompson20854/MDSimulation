15c875e267c44a268af453b99e477d64d264fc70
Steven Thompson <steventhompson@Stevens-MacBook-Pro.local>
2017-01-31 15:34:47 -0700

    fourth

--- NetBeansProjects/MDSimulation/src/mdsimulation/Particle.java
+++ NetBeansProjects/MDSimulation/src/mdsimulation/Particle.java
@@ -5,10 +5,7 @@
  */
 package mdsimulation;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
+
 
 /**
  *
@@ -27,7 +24,7 @@ import java.util.Scanner;
     int g;
     int b;
     
-   double[] values = new double[9];
+  
     
     
     public Particle(double rx, double ry, double vy, double vx,double mass, double radius, double r,
@@ -44,29 +41,10 @@ import java.util.Scanner;
         this.r = (int)r;
         this.g = (int)g;
         this.b = (int)b;
-        
-        //System.out.println(rx);
-        
-        values[0] = rx;
-        values[1] = ry;
-        values[2] = vx;
-        values[3] = vy;
-        values[4] = mass;
-        values[5] = radius;
-        values[6] = (int)r;
-        values[7] = (int)g;
-        values[8] = (int)b;
-      //for (int row = 0; row < values.length; row++)
-         //System.out.print(values[row] + " ");
   
-    //System.out.println(" ");
-       
     }
     
-    public void main(){
-     
-        
-    }
+  
     
 }
 
